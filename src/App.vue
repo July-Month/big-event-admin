dd9a46d2b582fbaba927f2fc47f50a9f28ddf050
July-Month <1345464419@qq.com>
2024-07-11 14:15:13 +0800

    husky配置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-
-// const obj = {
-//   name: 'sd',
-//   age: 18
-// }
 </script>
 
 <template>
